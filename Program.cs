e5c0ac1d9d30748e795a9a800100ee7fd5e41d04
Raphael David <raphaelruizd@gmail.com>
2025-04-06 19:26:28 +1000

    Edited program file to accomodate new changes

--- Program.cs
+++ Program.cs
@@ -5,7 +5,10 @@ class Program
     static void Main(string[] args)
     {
         // Create a new tool
+        Tool axe = new Tool("Axe", 7);
         Tool hammer = new Tool("Hammer", 10);
+        Tool screwdriver = new Tool("Screwdriver", 5);
+        Tool yardstick = new Tool("Yardstick", 3);
 
         // Display tool information
         Console.WriteLine($"Tool Name: {hammer.Name}");
@@ -34,7 +37,6 @@ class Program
         Console.WriteLine($"Available Quantity after returning: {hammer.AvailableQuantity}");
 
         //Testing Increment and Decrement methods
-        Tool screwdriver = new Tool("Screwdriver", 5);
         Console.WriteLine($"Current Amount of tools: {screwdriver.Quantity}");
         screwdriver.IncreaseQuantity(5);
         Console.WriteLine($"Current Amount of tools: {screwdriver.Quantity}");
@@ -43,10 +45,13 @@ class Program
 
         //Testing ToolCollection add and delete methods
         ToolCollection toolCollection = new ToolCollection(5);
-        toolCollection.Add(hammer);
         toolCollection.Add(screwdriver);
+        toolCollection.Add(hammer);
+        toolCollection.Add(axe);
+        toolCollection.Add(yardstick);
         Console.WriteLine($"Number of tools in collection: {toolCollection.Number}");
         Console.WriteLine($"Capacity of collection: {toolCollection.Capacity}");
+        Console.WriteLine($"Tools in collection: {string.Join(", ", toolCollection.Tools.Select(t => t.Name))}");
         toolCollection.Search(hammer);
         toolCollection.Delete(screwdriver);
         toolCollection.Search(screwdriver);
